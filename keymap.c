f3309df7c1687d2a48f92bcf382b9115e22c5fb8
core <core@ubuntu.home>
2019-01-21 14:38:54 +0100

    First Commit

---
+++ keymap.c
@@ -0,0 +1,137 @@
+﻿#include QMK_KEYBOARD_H
+#include "keymap_nordic.h"
+#include "keymap_swedish.h"
+
+extern keymap_config_t keymap_config;
+
+#define _QWERTY 0
+#define _LOWER 1
+#define _RAISE 2
+#define _ADJUST 16
+// #define KC_ KC_TRNS
+// #define _______ KC_TRNS
+
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  ADJUST,
+};
+
+enum my_keycodes {
+//Tap Dance Declarations
+  TD_Win_Exp= 0,
+
+ // Macros
+  KC_CCCV,
+  MY_CUSTOM_MACRO,
+};
+
+//Tap Dance Definitions
+qk_tap_dance_action_t tap_dance_actions[] = {
+
+  [TD_Win_Exp]  = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, LGUI(KC_E)),
+
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_QWERTY] = LAYOUT(
+  //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
+     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSPC,          KC_RBRC,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ENT,           KC_A,      KC_H,    KC_J,    KC_K,    KC_L,    NO_AA,  KC_ENT,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_A,             KC_A,      KC_N,    KC_M,  NO_OSLH,   NO_AE,  KC_LEFT, KC_UP,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_LCTL, TD(TD_Win_Exp), NO_ALGR, KC_LALT, LT(_LOWER, KC_COMM), KC_SPC, KC_SPC, LT(_RAISE, KC_COMM), KC_DOT, NO_MINS, KC_DOWN, KC_RGHT
+  //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
+  ),
+
+  [_LOWER] = LAYOUT(
+  //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
+     KC_GRV,  KC_1,    KC_PGUP,    KC_UP,    KC_4,    KC_5,    KC_DEL,             KC_B,    NO_MINS,    KC_7,    KC_8,    KC_9,    NO_ACUT,    KC_DEL,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_TAB,  KC_HOME,   KC_LEFT,   KC_DOWN,   KC_RGHT,   KC_END,   KC_F6,            KC_D,    NO_PLUS,    KC_4, KC_5,  KC_6, NO_QUOT, KC_PIPE,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_LSFT, KC_F7,   KC_PGDN,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           KC_SPC,  KC_N,    KC_1,    KC_2, KC_3,  NO_APOS, KC_ENT,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     _______, _______, _______, _______,           LOWER,   KC_SPC,           KC_SPC,  RAISE,            KC_0, NO_SLSH,   KC_DOWN, NO_PLUS
+  //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
+  ),
+
+  [_RAISE] = LAYOUT(
+  //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
+     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_A,             KC_B,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_C,             KC_D,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_A,             KC_SPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_LCTL, KC_LGUI, KC_LALT, ADJUST,           LOWER,   KC_SPC,           KC_SPC,  RAISE,            KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
+  //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
+  ),
+
+  [_ADJUST] = LAYOUT(
+  //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
+     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,            KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_BSPC,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_C,             KC_D,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_A,             KC_SPC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+  //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
+     KC_LCTL, KC_LGUI, KC_LALT, ADJUST,           LOWER,   KC_SPC,           KC_SPC,  RAISE,            KC_LEFT, KC_UP,   KC_DOWN, KC_RGHT
+  //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
+  )
+
+};
+
+
+
+
+
+
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_SONG(tone_qwerty);
+        #endif
+        set_single_persistent_default_layer(_QWERTY);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case ADJUST:
+      if (record->event.pressed) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
