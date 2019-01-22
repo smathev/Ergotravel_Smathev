e9fae7db104ffa8ca40fe75cd211af22ca476d79
smathev <sigurd.mathiasen@gmail.com>
2019-01-22 10:43:53 +0100

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -4,12 +4,12 @@
 
 extern keymap_config_t keymap_config;
 
+
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
 #define _ADJUST 16
 #define KC_ KC_TRNS
-//#define EMAIL 'sigurd.mathiasen"gmail.com'
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -19,29 +19,31 @@ enum custom_keycodes {
 };
 
 
-
+#define KC_closeShop ()
 enum my_keycodes {
 //Tap Dance Declarations
   TD_Win_Exp= 0,
 
  // Macros
-  MY_CCCV,
+  KC_CCCV,
   MY_CUSTOM_MACRO,
+  MAC_CLOSEWINDOW
 };
 
 //Tap Dance Definitions
 qk_tap_dance_action_t tap_dance_actions[] = {
 
   [TD_Win_Exp]  = ACTION_TAP_DANCE_DOUBLE(KC_LGUI, LGUI(KC_E)),
+
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
-     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSPC,          KC_VOLU,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC,
+     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSPC,          KC_RBRC,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ENT,           KC_VOLD,      KC_H,    KC_J,    KC_K,    KC_L,    NO_AA,  KC_ENT,
+     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ENT,           KC_A,      KC_H,    KC_J,    KC_K,    KC_L,    NO_AA,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_A,             KC_A,      KC_N,    KC_M,  NO_OSLH,   NO_AE,  KC_LEFT, KC_UP,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
@@ -63,11 +65,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT(
   //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
-     KC_ESC,  RESET,    KC_2,    KC_3,    KC_4,    KC_5,    KC_A,             KC_B,    KC_6,    MY_CCCV,    MY_CUSTOM_MACRO,    KC_9,    KC_0,    KC_BSPC,
+     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_A,             KC_B,    KC_6,    KC_CCCV,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_C,             KC_D,    KC_Y,    MY_CCCV,    KC_I,    KC_O,    KC_P,    KC_DEL,
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_C,             KC_D,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_A,             KC_SPC,  KC_N,    MY_CCCV,    KC_V,    KC_C,    KC_X,   KC_ENT,
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_A,             KC_SPC,  KC_N,    KC_M,    KC_V,    KC_C,    KC_X,   KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
      KC_,     KC_,    KC_,      KC_,     KC_,               KC_,             KC_,               KC_,     KC_LEFT, KC_UP,   KC_DOWN, KC_RCTL
   //`--------+--------+--------+--------+--------+--------+--------/        \--------+--------+--------+--------+--------+--------+--------'
@@ -130,27 +132,31 @@ switch (keycode) {
       }
       return false;
       break;
-    case MY_CCCV:                                  
+    case KC_CCCV:                                  
      if(record->event.pressed){
        code_timer= timer_read();
      } else {
-       if (timer_elapsed(code_timer) > TAPPING_TERM) { 
+       if (timer_elapsed(code_timer) > TAPPING_TERM) {   // Hold, copy
          SEND_STRING(SS_LALT("7") SS_LALT("0") SS_TAP(X_LEFT));
        } else { 
          SEND_STRING(SS_LALT("7"));
        }
      }
-     return false;
-     break;
-    case MY_CUSTOM_MACRO:
+    return false;
+    break;
+    case MAC_CLOSEWINDOW:                                  
      if(record->event.pressed){
-       SEND_STRING("Ø"); // this is our macro!
-      } 
-     return false;
-     break;
+       code_timer= timer_read();
+     } else {
+       if (timer_elapsed(code_timer) > TAPPING_TERM) {   // Hold, copy
+         SEND_STRING(SS_LALT(SS_TAP(F4)));
+       } 
+     }
+    return false;
+    break;
   }
   return true;
 }
-
+}
 
 
