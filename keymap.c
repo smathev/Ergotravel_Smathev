8723b960f97f7b22c9bc17b6f01791145fa6c026
smathev <sigurd.mathiasen@gmail.com>
2019-02-14 15:42:32 +0100

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -57,9 +57,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
   //,--------+--------+--------+--------+--------+--------+--------.        ,--------+--------+--------+--------+--------+--------+--------.
-     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSPC,          KC_RBRC,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC,
+     KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_BSPC,          KC_VOLU,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,   KC_BSPC,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
-     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ENT,           KC_A,      KC_H,    KC_J,    KC_K,    KC_L,    NO_AA,  KC_ENT,
+     KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_ENT,           KC_VOLD,      KC_H,    KC_J,    KC_K,    KC_L,    NO_AA,  KC_ENT,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_PGDOWN,         KC_PGUP,      KC_N,    KC_M,  NO_OSLH,   NO_AE,  KC_LEFT, KC_UP,
   //|--------+--------+--------+--------+--------+--------+--------|        |--------+--------+--------+--------+--------+--------+--------|
